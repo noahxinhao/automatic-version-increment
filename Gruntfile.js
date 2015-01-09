8ae13f893934a8f0744f864c0b0778f11ab17c00
noah <noahli@juxinli.com>
2015-01-09 18:45:10 +0800

    noah

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,55 +8,53 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+    // Project configuration.
+    grunt.initConfig({
 
-    // Configuration to be run (and then tested).
-    automatic: {
-      js: {
-        options: {
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp'],
         },
-    assetUrl:'demo/js/hello.js',
-        files: {
-          'tmp': ['demo/index.html'],
+
+        // Configuration to be run (and then tested).
+        automatic: {
+            js: {
+                options: {basicSrc: ["demo/js/"]},
+                assetUrl: ['hello.js'],
+                files: {
+                    'tmp': ['demo/index.html'],
+                },
+            },
+            css: {
+                options: {basicSrc: ["demo/css/"]},
+                assetUrl: ['hello.css'],
+                files: {
+                    'tmp': ['demo/index.html'],
+                },
+            },
         },
-      },
-      css: {
-        options: {
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js'],
         },
-    assetUrl:'demo/css/hello.css',
-        files: {
-          'tmp': ['demo/index.html'],
-        },
-      },
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+    });
 
-  });
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    //grunt.registerTask('test', ['clean', 'cache', 'nodeunit']);
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  //grunt.registerTask('test', ['clean', 'cache', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['automatic']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['automatic']);
 
 };
